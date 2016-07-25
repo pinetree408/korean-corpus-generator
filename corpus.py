b7b904e84f02c9063a6733791f6bcca0329a140b
Sangyoon Lee <pinetree408@gmail.com>
2016-07-25 16:02:18 +0900

    Change target corput

--- corpus.py
+++ corpus.py
@@ -4,26 +4,14 @@ import os
 
 
 def corpus_generator(path):
-    hangul = re.compile('[^ 가-힣]+')
     fr = open(path, 'r')
     lines = fr.readlines()
 
     result = []
-    result.append(path)
     for line in lines:
-        if line.strip() == '':
-            continue
-        sentence = line.strip().split('\n')[0]
-        if sentence[len(sentence)-1] == '.':
-            sentence_list = sentence.split('.')
-            for check in sentence_list:
-                if check.strip() == '':
-                    continue
-                initial = check.strip().decode('mbcs').encode('utf-8')
-                corpus = hangul.findall(initial)
-                if len(corpus) == 0:
-                    result.append(initial)
-
+        if ';' in line:
+            final = line.split(';')[1].split('\n')[0].strip()
+            result.append(final)
     fr.close()
     return result
 
@@ -34,18 +22,9 @@ def search_txt():
 
     result = []
     for filename in filenames:
-        if not('.py' in filename):
-            subfiles = os.listdir(dirname+filename+'/')
-            for subfile in subfiles:
-                if '.txt' in subfile:
-                    txtfile = dirname+filename+'/'+subfile
-                    result.append(txtfile)
-                elif not('.py' in subfile):
-                    finalfiles = os.listdir(dirname+filename+'/'+subfile+'/')
-                    for finalfile in finalfiles:
-                        if '.txt' in finalfile:
-                            txtfile = dirname+filename+'/'+subfile+'/'+finalfile
-                            result.append(txtfile)
+        if '.cdtree' in filename:
+            txtfile = dirname+filename
+            result.append(txtfile)
     return result
 
 
