f5dc38ac7e7e2ccc9511305d2095bfba620588aa
Sangyoon Lee <pinetree408@gmail.com>
2016-08-01 15:12:31 +0900

    Add randomize function

---
+++ random_select.py
@@ -0,0 +1,24 @@
+import random
+
+def random_select(filename):
+    fr = open(filename, 'r')
+    lines = fr.readlines()
+
+    fw = open('random_'+filename, 'w')
+
+    rand_selected = []
+    while len(rand_selected) != 50:
+        randomed = random.randint(0, len(lines)-1)
+        if not(randomed in rand_selected):
+            rand_selected.append(randomed)
+
+    for i in rand_selected:
+        fw.write(lines[i])
+
+    fw.close()
+    fr.close()
+
+random_select('short_pure.txt')
+random_select('short_pure_number.txt')
+random_select('short_pure_punctuation.txt')
+random_select('short_pure_number_punctuation.txt')
