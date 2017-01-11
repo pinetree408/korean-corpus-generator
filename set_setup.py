7b4b642f646dc62accdbab492d48e93fc91aa0d9
Sangyoon Lee <pinetree408@gmail.com>
2017-01-11 13:57:13 +0900

    Fix code convention of set_setup module with pylint

--- set_setup.py
+++ set_setup.py
@@ -1,20 +1,29 @@
-from modules import corpus, shorter, random_selecter, analyze
-from util import correlation_calc
+# -*- coding: utf-8 -*-
+"""Set up the initial set (complex, 4 parent pharse sets)
+This module generate complex and 4 parent pharse sets
+"""
+
 import os
-import shutil
+from modules import corpus, shorter
 
-set_path = './set/'
-if not os.path.exists(set_path):
-    os.makedirs(set_path)
+def main():
+    """main srcipt for generate initial set for analayzing
+    """
+    set_path = './set/'
+    if not os.path.exists(set_path):
+        os.makedirs(set_path)
 
-print "Start"
-print "Complex start"
-complex = corpus.Complex()
-complex.generate(set_path)
-print "Complex complete"
-print "Pre-process start"
-preprocess_first = corpus.Preprocess()
-preprocess_first.generator(set_path)
-preprocess_second = shorter.Preprocess()
-preprocess_second.generator(set_path)
-print "Pre-process complete"
+    print "Start"
+    print "Complex start"
+    complex_set = corpus.Complex()
+    complex_set.generate(set_path)
+    print "Complex complete"
+    print "Pre-process start"
+    preprocess_first = corpus.Preprocess()
+    preprocess_first.generator(set_path)
+    preprocess_second = shorter.Preprocess()
+    preprocess_second.generator(set_path)
+    print "Pre-process complete"
+
+if __name__ == "__main__":
+    main()
