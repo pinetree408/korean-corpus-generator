0f4e6f4017b015a8e07a66974fd2063ebab7505d
Sangyoon Lee <pinetree408@gmail.com>
2017-01-12 12:06:22 +0900

    Fix code convention of shorter module

--- modules/shorter.py
+++ modules/shorter.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
+"""Make Short corpus from 4 parent pharse
+
+This module generate short corpus from 4 parent pharse
+
+"""
 import re
-import copy
-import codecs
 import os
 
-class KE():
+class KE(object):
+    """korean & english regular expersion analyze class
+    Attributes:
+        en_h (str): Header of korean
+        en_b (dict): Body of korean
+        en_f (dict): Footer of korean
+        reg_h (str): regular expression of korean Header
+        reg_b (str): regular expression of korean Body
+        reg_f (str): regular expression of korean footer
+    """
 
-    enH = "rRseEfaqQtTdwWczxvg"
-    enB_list = [
+    en_h = "rRseEfaqQtTdwWczxvg"
+    en_b_list = [
         'k', 'o', 'i', 'O', 'j',
         'p', 'u', 'P', 'h', 'hk',
         'ho', 'hl', 'y', 'n', 'nj',
         'np', 'nl', 'b', 'm', 'ml',
         'l'
     ]
-    enF_list = [
+    en_f_list = [
         '', 'r', 'R', 'rt', 's',
         'sw', 'sg', 'e', 'f', 'fr',
         'fa', 'fq', 'ft', 'fx', 'fv',
@@ -23,75 +35,95 @@ class KE():
         'x', 'v', 'g'
     ]
 
-    regH = "[rRseEfaqQtTdwWczxvg]"
-    regB = "hk|ho|hl|nj|np|nl|ml|k|o|i|O|j|p|u|P|h|y|n|b|m|l"
-    regF = "rt|sw|sg|fr|fa|fq|ft|fx|fv|fg|qt|r|R|s|e|f|a|q|t|T|d|w|c|z|x|v|g|"
+    reg_h = "[rRseEfaqQtTdwWczxvg]"
+    reg_b = "hk|ho|hl|nj|np|nl|ml|k|o|i|O|j|p|u|P|h|y|n|b|m|l"
+    reg_f = "rt|sw|sg|fr|fa|fq|ft|fx|fv|fg|qt|r|R|s|e|f|a|q|t|T|d|w|c|z|x|v|g|"
 
     def __init__(self):
-        enB_dict = {}
-        for i in range(len(self.enB_list)):
-            enB_dict[self.enB_list[i]] = i
-        enF_dict = {}
-        for i in range(len(self.enF_list)):
-            enF_dict[self.enF_list[i]] = i
-        self.enB = enB_dict
-        self.enF = enF_dict
-        regH_block = "("+self.regH+")"
-        regB_block = "("+self.regB+")"
-        regF_item_first = "("+self.regF+")"
-        regF_item_second = "(?=("+self.regH+")("+self.regB+"))|("+self.regF+")"
-        regF_block = "(" + regF_item_first + regF_item_second + ")"
-        self.regex = regH_block + regB_block + regF_block
+        """Initialize KE class
+        this method initialize all attributes of this class
+        """
+        reg_h_block = "("+self.reg_h+")"
+        reg_b_block = "("+self.reg_b+")"
+        reg_f_item_first = "("+self.reg_f+")"
+        reg_f_item_second = "(?=("+self.reg_h+")("+self.reg_b+"))|("+self.reg_f+")"
+        reg_f_block = "(" + reg_f_item_first + reg_f_item_second + ")"
+        self.regex = reg_h_block + reg_b_block + reg_f_block
+        self.hangul = re.compile('[^가-힣]+')
 
     def change_complete_korean(self, word):
-
-        hangul = re.compile('[^가-힣]+')
-        additional = hangul.findall(word)
+        """chnage korean word to korean letter list
+        Args:
+            word (str): target word
+        Return:
+            len (int): length of target word's letter
+        """
+        additional = self.hangul.findall(word)
         additional_len = 0
         for item in additional:
             additional_len += len(item)
-        word = hangul.sub('', word).decode('utf-8')
+        words = self.hangul.sub('', word).decode('utf-8')
         result = 0
-        for i in range(len(word)):
-            char_code = ord(word[i])
+        for word in words:
+            char_code = ord(word)
             if char_code < 44032 or char_code > 55203:
                 break
             char_code = char_code - 44032
-            enH_code = char_code / 588
-            enBF_code = char_code % 588
-            enB_code = enBF_code / 28
-            enF_code = enBF_code % 28
-            enH_char = self.enH[enH_code]
-            enB_char = self.enB_list[enB_code]
-            enF_char = self.enF_list[enF_code]
+            en_h_code = char_code / 588
+            en_bf_code = char_code % 588
+            en_b_code = en_bf_code / 28
+            en_f_code = en_bf_code % 28
+            en_h_char = self.en_h[en_h_code]
+            en_b_char = self.en_b_list[en_b_code]
+            en_f_char = self.en_f_list[en_f_code]
 
-            result += len(enH_char)
-            result += len(enB_char)
-            result += len(enF_char)
+            result += self.sum_length(en_h_char, en_b_char, en_f_char)
 
         return result + additional_len
 
-class Preprocess():
+    @staticmethod
+    def sum_length(en_h, en_b, en_f):
+        """sum enh, enb, enf's length
+        Args:
+            en_h (str): enh's char
+            en_b (str): enb's char
+            en_f (str): enf's char
+        """
+        return len(en_h) + len(en_b) + len(en_f)
 
+class Preprocess(object):
+    """generate short corpus from 4 parent pharses
+    Attributes:
+        ke (object): korean & english object
+    """
     def __init__(self):
-        self.ke = KE()
+        """Initialize preprocess class
+        this method initialize all attributes of this class
+        """
+        self.ke_object = KE()
 
     def shorter(self, set_path, filename):
-  
-        fr = open(set_path + filename, 'r')
-        fw = open(set_path + 'short/short_' + filename, 'w')
-        lines = fr.readlines()
-        for line in lines:
-            splited = line[:len(line)-1].split(' ')
-            splited_len = len(splited) - 1
-            for item in splited:
-                splited_len += self.ke.change_complete_korean(item)
-            if splited_len > 20 and splited_len < 50:
-                fw.write(line)
-        fw.close()
-        fr.close()
+        """generate short pharse set from target file
+        Args:
+            set_path (str): target file's path
+            filename (str): target file name
+        """
+        with open(set_path + filename, 'r') as file_read:
+            with open(set_path + 'short/short_' + filename, 'w') as file_write:
+                lines = file_read.readlines()
+                for line in lines:
+                    splited = line[:len(line)-1].split(' ')
+                    splited_len = len(splited) - 1
+                    for item in splited:
+                        splited_len += self.ke_object.change_complete_korean(item)
+                    if splited_len > 20 and splited_len < 50:
+                        file_write.write(line)
 
     def generator(self, set_path):
+        """generate short pharse set from each parent set
+        Args:
+            set_path (str): target file's path
+        """
         if not os.path.exists(set_path + "short"):
             os.makedirs(set_path + "short")
         self.shorter(set_path, 'pure.txt')
